61990fcb047166ff961d749c84b9b4e47c812f22
alex <argriffi@ncsu.edu>
2013-11-10 17:30:07 -0500

    iris data

---
+++ README.md
@@ -0,0 +1,5 @@
+
+```
+$ od --format=d4 --width=4 --output-duplicates iris.predict
+$ od --format=f8 --width=40 --output-duplicates iris.respond
+```
---
+++ init-iris-data.py
@@ -0,0 +1,68 @@
+"""
+Write some binary files.
+iris.predict
+iris.respond
+"""
+
+import itertools
+import struct
+
+import statsmodels
+import statsmodels.api as st
+
+IRIS_PREDICTOR_COUNT = 4
+
+
+# Copypasted directly from official itertools recipes.
+def unique_everseen(iterable, key=None):
+    "List unique elements, preserving order. Remember all elements ever seen."
+    # unique_everseen('AAAABBBCCDAABBB') --> A B C D
+    # unique_everseen('ABBCcAD', str.lower) --> A B C D
+    seen = set()
+    seen_add = seen.add
+    if key is None:
+        for element in itertools.ifilterfalse(seen.__contains__, iterable):
+            seen_add(element)
+            yield element
+    else:
+        for element in iterable:
+            k = key(element)
+            if k not in seen:
+                seen_add(k)
+                yield element
+
+
+def main():
+    """
+    Use the famous iris data set.
+    """
+
+    # Read some data.
+    iris = statsmodels.datasets.get_rdataset('iris', 'datasets')
+    y = iris.data.Species
+    print y.values
+    x = iris.data.ix[:, :IRIS_PREDICTOR_COUNT]
+    x = st.add_constant(x, prepend=False)
+    print x.values
+
+    # Map labels from strings to integers.
+    index_to_label = list(unique_everseen(y.values))
+    label_to_index = dict((x, i) for i, x in enumerate(index_to_label))
+
+    # Write the binary file that has the prediction data.
+    with open('iris.predict', 'wb') as fout_predict:
+        fmt = 'i'
+        for label in y.values:
+            idx = label_to_index[label]
+            fout_predict.write(struct.pack(fmt, idx))
+
+    with open('iris.respond', 'wb') as fout_respond:
+        fmt = 'd'
+        for row in x.values:
+            for v in row:
+                fout_respond.write(struct.pack(fmt, float(v)))
+
+
+if __name__ == '__main__':
+    main()
+
