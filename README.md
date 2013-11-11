3af6547ba09093cb7672d04034e28f4fe9a0d4c1
alex <argriffi@ncsu.edu>
2013-11-10 20:31:54 -0500

    amino acid data processor

--- README.md
+++ README.md
@@ -11,6 +11,14 @@ $ od --format=d4 --width=4 --output-duplicates iris.respond
 ```
 
 ```
+aa data:
+
+34009810 observations
+5 predictors including the intercept
+400 categories including the reference category
+```
+
+```
 how to build:
 
 $ gcc -o uglogit uglogit.c <path-to>/lbfgs.o -I<path-to>/liblbfgs/include -lm
--- init-aa-data.py
+++ init-aa-data.py
@@ -1,16 +1,16 @@
 """
-Write some binary files.
-iris.predict
-iris.respond
+Convert a file into binary outputs.
+
+The first column is assumed to be the response label.
+The remaining columns are assumed to be predictors.
+An intercept is added to the predictor set.
+
 """
 
 import itertools
+import sys
 import struct
-
-import statsmodels
-import statsmodels.api as st
-
-IRIS_PREDICTOR_COUNT = 4
+import csv
 
 
 # Copypasted directly from official itertools recipes.
@@ -31,41 +31,44 @@ def unique_everseen(iterable, key=None):
                 seen_add(k)
                 yield element
 
-
 def main():
-    """
-    Use the famous iris data set.
-    """
 
-    # Read some data.
-    iris = statsmodels.datasets.get_rdataset('iris', 'datasets')
-    y = iris.data.Species
-    print y.values
-    x = iris.data.ix[:, :IRIS_PREDICTOR_COUNT]
-    x = st.add_constant(x, prepend=False)
-    print x.values
+    # read the csv file from stdin
+    endog = []
+    exog = []
+    reader = csv.reader(sys.stdin, delimiter='\t')
+    for row in reader:
+        endog_element = row[0]
+        exog_row = [float(x) for x in row[1:]] + [1.0]
+        endog.append(endog_element)
+        exog.append(exog_row)
 
     # Map labels from strings to integers.
-    index_to_label = list(unique_everseen(y.values))
+    index_to_label = list(unique_everseen(endog))
     label_to_index = dict((x, i) for i, x in enumerate(index_to_label))
 
     # Write the binary file that has the prediction data.
-    with open('iris.predict', 'wb') as fout_predict:
-        fmt = 'd'
-        for row in x.values:
-            for v in row:
-                fout_predict.write(struct.pack(fmt, float(v)))
+    print 'processing prediction data...'
+    with open('aa.predict', 'wb') as fout_predict:
+        fmt = 'd' * len(exog[0])
+        for i, row in enumerate(exog):
+            if not (i+1) % 100000:
+                print 'row', i+1, 'of', len(exog)
+            fout_predict.write(struct.pack(fmt, *row))
 
     # Write the binary file that has the response data.
-    with open('iris.respond', 'wb') as fout_respond:
+    print 'processing response data...'
+    with open('aa.respond', 'wb') as fout_respond:
         fmt = 'i'
-        for label in y.values:
+        for i, label in enumerate(endog):
+            if not (i+1) % 100000:
+                print 'row', i+1, 'of', len(exog)
             idx = label_to_index[label]
             fout_respond.write(struct.pack(fmt, idx))
 
     # Report a few numbers.
-    print len(y.values), 'observations'
-    print x.values.shape[1], 'predictors including the intercept'
+    print len(endog), 'observations'
+    print len(exog[0]), 'predictors including the intercept'
     print len(label_to_index), 'categories including the reference category'
 
 
